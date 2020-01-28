bbedfcc8e9217211532b6d47c4990b8b7aeb767c
Moa Persson <moa@irf.se>
2020-01-28 14:30:13 +0100

    new script for comparing boundaries determined from same orbit by different users.

---
+++ source/ven03_extractboundarycomparisonbetweenusers.py
@@ -0,0 +1,17 @@
+"""
+Extract the determined boundaries but compare the boundaries determined for
+the same orbits between the different users. How much do they differ and why?
+"""
+
+import numpy as np
+import datetime
+import sys
+
+
+def main():
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
