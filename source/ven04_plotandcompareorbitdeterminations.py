30f2cf727c60ae79423c2b4c9721abde0489f574
Moa Persson <moa@irf.se>
2020-02-11 11:03:41 +0100

    Script for plotting all users determinations to understand if they agree and/or is correct.

---
+++ source/ven04_plotandcompareorbitdeterminations.py
@@ -0,0 +1,20 @@
+"""
+Plot the determined times for the orbits for the students.
+
+Do I agree with their determinations?
+What is the difference between the different students?
+
+"""
+
+import numpy as np
+import datetime
+import sys
+
+
+def main():
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
